dcecab313c1e27e39fac96cadda5ac2fea05207c
Jim Hartric <jim@et.com>
2017-02-15 18:50:06 +0100

    deploy(dist): deploy on GitHub pages

--- out-tsc/app/gym.spec.js
+++
@@ -1,7 +0,0 @@
-import { Gym } from './gym';
-describe('Gym', function () {
-    it('should create an instance', function () {
-        expect(new Gym()).toBeTruthy();
-    });
-});
-//# sourceMappingURL=W:/Code/sonkal/qr-code/src/app/gym.spec.js.map
--- out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=W:/Code/sonkal/qr-code/src/environments/environment.prod.js.map
